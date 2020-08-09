90af50ad9640c9efb323bc082e6d0003fa24e852
Victor Raposo <69219137+vhraposo@users.noreply.github.com>
2020-08-09 19:05:55 -0300

    Add files via upload

---
+++ src/database/fake_data.js
@@ -0,0 +1,37 @@
+//Dados
+const proffys = [
+    { 
+        name:"Diego Fernandes", 
+        avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", whatsapp:"4387635267",
+        bio:"Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        //se der problema de dados acima, trocar por aspas simples 
+        subject: "Química", 
+        cost:"20", 
+        weekday: [0], 
+        time_from:[720], 
+        time_to: [1220]
+    },
+    { 
+        name:"Daniele Evagenlista", 
+        avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", whatsapp:"4387635267",
+        bio:"Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        //se der problema de dados acima, trocar por aspas simples 
+        subject: "Química", 
+        cost:"20", 
+        weekday: [1], 
+        time_from:[720], 
+        time_to: [1220]
+    },
+    { 
+        name:"Victor Raposo", 
+        avatar:"https://avatars0.githubusercontent.com/u/69219137?s=460&u=b3ef7daf0a4e29af8fd83c65c4a9a712e742076d&v=4", 
+        whatsapp:"4387635267",
+        bio:"Entusiasta das melhores tecnologias de química avançada.Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        //se der problema de dados acima, trocar por aspas simples 
+        subject: "Javascript", 
+        cost:"20", 
+        weekday: [0], 
+        time_from:[720], 
+        time_to: [1220]
+    }
+]
